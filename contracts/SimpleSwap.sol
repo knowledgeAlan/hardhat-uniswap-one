38e08b92037fac384734a5779618785566807f5b
zhongzuomin <1299076979@qq.com>
2024-02-25 21:47:24 +0800

    Update SimpleSwap.sol

--- contracts/SimpleSwap.sol
+++ contracts/SimpleSwap.sol
@@ -7,6 +7,7 @@ import '@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol';
 
 contract SimpleSwap {
     ISwapRouter public immutable swapRouter;
+    address public constant LINK;
     address public constant DAI = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
     address public constant WETH9 = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
     uint24 public constant feeTier = 3000;
